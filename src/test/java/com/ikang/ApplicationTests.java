a4bd22b27d8c7911c444bc2a132e5bce41ed9ef5
徐洋 <yang.xu@ikang.com>
2019-11-07 10:32:00 +0800

    update pom.xml

--- src/test/java/com/ikang/ApplicationTests.java
+++ src/test/java/com/ikang/ApplicationTests.java
@@ -1,13 +1,12 @@
 package com.ikang;
 
-import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest
-class ApplicationTests {
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
